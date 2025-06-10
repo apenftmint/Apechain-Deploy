ad4f375af47e6de690f71226d1bfe78255181ea5
apenftmint <nftmintplatform@gmail.com>
2025-06-10 14:27:08 +0530

    Initial deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // 👈 Important for Netlify, makes paths relative
+    base: '/', // 👈 Important for Netlify, makes paths relative
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
