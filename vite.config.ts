8b06c7a6d3a56b570319b67c150199f6c505d569
apenftmint <nftmintplatform@gmail.com>
2025-06-10 17:23:58 +0530

    Initial commit 🚀

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: '/', // 👈 Use this for Netlify root domain
+    base: '/Apechain-Deploy/', // 👈 Use this for Netlify root domain
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
